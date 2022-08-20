5fbbd3eae5fccc6ffb104cd86366e158e4181d5e
Cauã Hiroshi Dias Tamari <53350106+cauahiroshi@users.noreply.github.com>
2022-08-20 15:31:46 -0300

    Add files via upload

---
+++ scripts.js
@@ -0,0 +1,16 @@
+alert ("No site você vai encontrar todos os tópicos que vão ser trabalhados no Workshop :) ")
+
+
+/*let minhaImagem = document.querySelector('img');
+
+minhaImagem.onclick = function() {
+    let meuSrc = imagem.getAttribute('src');
+    if(meuSrc === 'images/firefox-icon.png') {
+      minhaImagem.setAttribute ('src','images/firefox2.png');
+    } else {
+      minhaImagem.setAttribute ('src','images/firefox-icon.png');
+    }
+}*/
+
+
+
